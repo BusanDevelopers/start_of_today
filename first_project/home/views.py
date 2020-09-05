2ae05c6d432d4fdbede44be4889a7489982b5e98
yeongwoo-Cho <jyy0223@naver.com>
2020-09-05 16:23:01 +0900

    complate

--- first_project/home/views.py
+++ first_project/home/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from .weather import today, news
 from .get_wise import get_Wise
 # Create your views here.
 
 
 def home(request):
-    article = news()
-    weather = today()
     wise_saying = get_Wise()
 
-    return render(request, "home.html", {'article': article, 'weather': weather, 'wise_saying': wise_saying})
+    return render(request, "home.html", {'wise_saying': wise_saying})
