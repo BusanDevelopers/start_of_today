969ef3065470ea3177a65d1b2db6bee03a09db0b
yeongwoo-Cho <jyy0223@naver.com>
2020-09-18 19:32:44 +0900

    upgrade security

--- first_project/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .get_wise import get_Wise
-# Create your views here.
-
-
-def home(request):
-    wise_saying = get_Wise()
-
-    return render(request, "home.html", {'wise_saying': wise_saying})
--- first_project/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('motivation/', views.motivation),
-    path('job/', views.career),
-    path('coding/', views.coding),
-]
